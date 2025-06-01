6177c7810aebf43d85309d36209c65f57979533a
Umesh Patil <147147544+Umesh0903@users.noreply.github.com>
2025-06-01 17:42:20 +0530

    Add files via upload

---
+++ DatabaseConnectivity1/mydata/Form1.Designer.cs
@@ -0,0 +1,202 @@
+﻿namespace DatabaseConnectivity1
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.rollno = new System.Windows.Forms.Label();
+            this.rollnotxt = new System.Windows.Forms.TextBox();
+            this.name = new System.Windows.Forms.Label();
+            this.nametxt = new System.Windows.Forms.TextBox();
+            this.city = new System.Windows.Forms.Label();
+            this.citytxt = new System.Windows.Forms.TextBox();
+            this.addrecordbtn = new System.Windows.Forms.Button();
+            this.cancelbtn = new System.Windows.Forms.Button();
+            this.updatebtn = new System.Windows.Forms.Button();
+            this.deletebtn = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Location = new System.Drawing.Point(49, 46);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.Size = new System.Drawing.Size(254, 150);
+            this.dataGridView1.TabIndex = 0;
+            this.dataGridView1.CellClick += new System.Windows.Forms.DataGridViewCellEventHandler(this.dataGridView1_CellClick);
+            this.dataGridView1.CellContentClick += new System.Windows.Forms.DataGridViewCellEventHandler(this.dataGridView1_CellContentClick);
+            // 
+            // rollno
+            // 
+            this.rollno.AutoSize = true;
+            this.rollno.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.rollno.Location = new System.Drawing.Point(440, 46);
+            this.rollno.Name = "rollno";
+            this.rollno.Size = new System.Drawing.Size(59, 16);
+            this.rollno.TabIndex = 1;
+            this.rollno.Text = "RollNo:";
+            // 
+            // rollnotxt
+            // 
+            this.rollnotxt.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.rollnotxt.Location = new System.Drawing.Point(521, 46);
+            this.rollnotxt.Multiline = true;
+            this.rollnotxt.Name = "rollnotxt";
+            this.rollnotxt.Size = new System.Drawing.Size(136, 26);
+            this.rollnotxt.TabIndex = 2;
+            // 
+            // name
+            // 
+            this.name.AutoSize = true;
+            this.name.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.name.Location = new System.Drawing.Point(440, 94);
+            this.name.Name = "name";
+            this.name.Size = new System.Drawing.Size(52, 16);
+            this.name.TabIndex = 3;
+            this.name.Text = "Name:";
+            // 
+            // nametxt
+            // 
+            this.nametxt.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.nametxt.Location = new System.Drawing.Point(521, 93);
+            this.nametxt.Multiline = true;
+            this.nametxt.Name = "nametxt";
+            this.nametxt.Size = new System.Drawing.Size(136, 26);
+            this.nametxt.TabIndex = 4;
+            // 
+            // city
+            // 
+            this.city.AutoSize = true;
+            this.city.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.city.Location = new System.Drawing.Point(440, 146);
+            this.city.Name = "city";
+            this.city.Size = new System.Drawing.Size(37, 16);
+            this.city.TabIndex = 5;
+            this.city.Text = "City:";
+            // 
+            // citytxt
+            // 
+            this.citytxt.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.citytxt.Location = new System.Drawing.Point(521, 146);
+            this.citytxt.Multiline = true;
+            this.citytxt.Name = "citytxt";
+            this.citytxt.Size = new System.Drawing.Size(136, 26);
+            this.citytxt.TabIndex = 6;
+            // 
+            // addrecordbtn
+            // 
+            this.addrecordbtn.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.addrecordbtn.Location = new System.Drawing.Point(466, 198);
+            this.addrecordbtn.Name = "addrecordbtn";
+            this.addrecordbtn.Size = new System.Drawing.Size(75, 23);
+            this.addrecordbtn.TabIndex = 7;
+            this.addrecordbtn.Text = "ADD Record";
+            this.addrecordbtn.UseVisualStyleBackColor = true;
+            this.addrecordbtn.Click += new System.EventHandler(this.addrecord_Click);
+            // 
+            // cancelbtn
+            // 
+            this.cancelbtn.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.cancelbtn.Location = new System.Drawing.Point(582, 198);
+            this.cancelbtn.Name = "cancelbtn";
+            this.cancelbtn.Size = new System.Drawing.Size(75, 23);
+            this.cancelbtn.TabIndex = 8;
+            this.cancelbtn.Text = "Cancel";
+            this.cancelbtn.UseVisualStyleBackColor = true;
+            this.cancelbtn.Click += new System.EventHandler(this.cancelbtn_Click);
+            // 
+            // updatebtn
+            // 
+            this.updatebtn.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.updatebtn.Location = new System.Drawing.Point(466, 249);
+            this.updatebtn.Name = "updatebtn";
+            this.updatebtn.Size = new System.Drawing.Size(75, 23);
+            this.updatebtn.TabIndex = 9;
+            this.updatebtn.Text = "Update";
+            this.updatebtn.UseVisualStyleBackColor = true;
+            this.updatebtn.Click += new System.EventHandler(this.updatebtn_Click);
+            // 
+            // deletebtn
+            // 
+            this.deletebtn.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.deletebtn.Location = new System.Drawing.Point(582, 249);
+            this.deletebtn.Name = "deletebtn";
+            this.deletebtn.Size = new System.Drawing.Size(75, 23);
+            this.deletebtn.TabIndex = 10;
+            this.deletebtn.Text = "Delete";
+            this.deletebtn.UseVisualStyleBackColor = true;
+            this.deletebtn.Click += new System.EventHandler(this.deletebtn_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.deletebtn);
+            this.Controls.Add(this.updatebtn);
+            this.Controls.Add(this.cancelbtn);
+            this.Controls.Add(this.addrecordbtn);
+            this.Controls.Add(this.citytxt);
+            this.Controls.Add(this.city);
+            this.Controls.Add(this.nametxt);
+            this.Controls.Add(this.name);
+            this.Controls.Add(this.rollnotxt);
+            this.Controls.Add(this.rollno);
+            this.Controls.Add(this.dataGridView1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.Label rollno;
+        private System.Windows.Forms.TextBox rollnotxt;
+        private System.Windows.Forms.Label name;
+        private System.Windows.Forms.TextBox nametxt;
+        private System.Windows.Forms.Label city;
+        private System.Windows.Forms.TextBox citytxt;
+        private System.Windows.Forms.Button addrecordbtn;
+        private System.Windows.Forms.Button cancelbtn;
+        private System.Windows.Forms.Button updatebtn;
+        private System.Windows.Forms.Button deletebtn;
+    }
+}
+
---
+++ DatabaseConnectivity1/mydata/Form1.cs
@@ -0,0 +1,156 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Data.SqlClient;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Xml.Linq;
+using Microsoft.Data.SqlClient;
+using SqlCommand = Microsoft.Data.SqlClient.SqlCommand;
+using SqlDataAdapter = Microsoft.Data.SqlClient.SqlDataAdapter;
+
+
+namespace DatabaseConnectivity1
+{
+    public partial class Form1: Form
+    {
+        Microsoft.Data.SqlClient.SqlConnection con = null;
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void addrecord_Click(object sender, EventArgs e)
+        {
+            AddData();
+            getData();
+        }
+
+        private void cancelbtn_Click(object sender, EventArgs e)
+        {
+          rollnotxt.Text = nametxt.Text = citytxt.Text = "";
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            /*string connectionString = "Data Source=DESKTOP-RJ7FDJE\\SQLEXPRESS;Initial Catalog=mydb;Integrated Security=True;Encrypt=True;Trust Server Certificate=True";
+            string query = "select * from student";
+            con = new SqlConnection(connectionString);
+            con.Open();
+
+            SqlDataAdapter sda = new SqlDataAdapter(query, connectionString);
+            DataSet ds = new DataSet();
+            sda.Fill(ds, "student");
+
+            dataGridView1.DataSource = ds.Tables["student"].DefaultView;
+            con.Close();*/
+            getData();
+          
+
+        }
+
+        /*public SqlConnection GetCon()
+        {
+            return con;
+        }*/
+
+         public void getData()
+         {
+            string connectionString = "Data Source=DESKTOP-RJ7FDJE\\SQLEXPRESS;Initial Catalog=mydb;Integrated Security=True;Trust Server Certificate=True";
+            string query = "select * from student";
+            con = new Microsoft.Data.SqlClient.SqlConnection(connectionString);
+            con.Open();
+
+            SqlDataAdapter sda = new SqlDataAdapter(query, connectionString);
+            DataSet ds = new DataSet();
+            sda.Fill(ds, "student");
+
+            dataGridView1.DataSource = ds.Tables["student"].DefaultView;
+
+            con.Close();
+         }
+
+        public void AddData()    
+        {
+            string rollno, name, city;
+            rollno = rollnotxt.Text;
+            name = nametxt.Text;
+            city = citytxt.Text;
+            int rno = Convert.ToInt32(rollno);
+            con.Open();
+            SqlCommand cmd = new SqlCommand("insert into student values(@rollno,@name,@city)", con);
+
+            cmd.CommandType = CommandType.Text;
+            cmd.Parameters.AddWithValue("@rollno", rno);
+            cmd.Parameters.AddWithValue("@name", name);
+            cmd.Parameters.AddWithValue("@city", city);
+
+            int count = cmd.ExecuteNonQuery();
+            MessageBox.Show(count + " Record added successfully", "Record added", MessageBoxButtons.OK, MessageBoxIcon.Information);
+            con.Close();
+        }
+
+        public void UpdateData()
+        {
+            string rollno, name, city;
+            rollno = rollnotxt.Text;
+            name = nametxt.Text;
+            city = citytxt.Text;
+            int rno = Convert.ToInt32(rollno);
+            con.Open();
+            SqlCommand cmd = new SqlCommand("update student set name=@name, city=@city where rollno=@rollno", con);
+
+            cmd.CommandType = CommandType.Text;
+            cmd.Parameters.AddWithValue("@rollno", rno);
+            cmd.Parameters.AddWithValue("@name", name);
+            cmd.Parameters.AddWithValue("@city", city);
+
+            int count = cmd.ExecuteNonQuery();
+            MessageBox.Show(count + " Record updated successfully", "Record updated", MessageBoxButtons.OK, MessageBoxIcon.Information);
+            con.Close();
+
+        }
+
+        public void DeleteData()
+        {
+            con.Open();
+            int rollno = Convert.ToInt32(rollnotxt.Text);
+            SqlCommand cmd = new SqlCommand("delete from student where rollno=@rollno", con);
+
+            cmd.CommandType = CommandType.Text;
+            cmd.Parameters.AddWithValue("@rollno", rollno);
+
+            int count = cmd.ExecuteNonQuery();
+            MessageBox.Show(count + " Record deleted successfully", "Record deleted", MessageBoxButtons.OK, MessageBoxIcon.Information);
+            con.Close();
+        }
+
+        private void dataGridView1_CellContentClick(object sender, DataGridViewCellEventArgs e)
+        {
+
+        }
+
+        private void updatebtn_Click(object sender, EventArgs e)  //update button
+        {
+            UpdateData();
+        }
+
+        private void deletebtn_Click(object sender, EventArgs e)    //delete button
+        {
+            DeleteData();
+        }
+
+        private void dataGridView1_CellClick(object sender, DataGridViewCellEventArgs e)
+        {
+            rollnotxt.Text = dataGridView1.Rows[dataGridView1.CurrentRow.Index].Cells[0].Value.ToString();
+            nametxt.Text = dataGridView1.Rows[dataGridView1.CurrentRow.Index].Cells[1].Value.ToString();
+            citytxt.Text = dataGridView1.Rows[dataGridView1.CurrentRow.Index].Cells[2].Value.ToString();
+            rollnotxt.ReadOnly = true;
+
+        }
+    }
+}
